72ce6607d65a59f8d2c64bc93eda918b87999761
Quinn Daley <quinn@fishpercolator.co.uk>
2023-12-07 11:47:24 +0000

    Bugs fixed I finally solved part 1

--- 07/07-1.rb
+++ 07/07-1.rb
@@ -1,23 +1,20 @@
 #!/usr/bin/env ruby
 
 class Hand
-  TYPES = [/(.)\1{4}/, /(.)\1{3}/, /(.)\1\1(.)\2/, /(.)\1\1/, /(.)\1(.?)(.)\3/, /(.)\1/, /(.)/].freeze
+  TYPES = [/(.)\1{4}/, /(.)\1{3}/, /^(.)\1\1?(.)\2\2?$/, /(.)\1\1/, /(.)\1.?(.)\2/, /(.)\1/, /(.)/].freeze
   CRANKS = "AKQJT98765432".split('')
 
-  attr_reader :cards
+  attr_reader :cards, :cards_sorted
 
   def initialize(cards)
-    @cards = cards.split('').sort_by {CRANKS.index(_1)}.join
+    @cards = cards.split('')
+    @cards_sorted = @cards.sort_by {CRANKS.index(_1)}.join
   end
 
   # Returns a series of integers that can be used to score the hand - lower is better.
   def scores
-    type = TYPES.each_index.find {TYPES[_1] =~ cards}
-
-    # Type 4 has three capture groups, so handle it differently
-    winning_cards = (type == 4 ? [$1,$3] : [$1,$2]).compact
-    non_winning_cards = ($`+(type == 4 ? $2 : '')+$').split('').uniq
-    [type] + (winning_cards + non_winning_cards).map {CRANKS.index(_1)}
+    type = TYPES.each_index.find {TYPES[_1] =~ cards_sorted}
+    [type] + cards.map {CRANKS.index(_1)}
   end
 
   # Puts lower value hands at the start
@@ -28,12 +25,12 @@ class Hand
   # For debugging
   def inspect
     types = %i[five four fullhouse three twopair pair highcard]
-    s = scores
-    "#{cards} #{types[s.shift]} #{s.map {CRANKS[_1]}.inspect}"
+    "#{cards.join} #{types[scores.shift]} (#{cards_sorted})"
   end
 end
 
 hands = ARGF.inject({}) {|h,l| l.split(' ').then {h[Hand.new _1] = _2.to_i; h}}
-hands.keys.each {p _1}
-# winnings = hands.keys.sort.map.with_index {|h,i| hands[h] * (hands.length-i)}
-# p winnings.sum
+hands_in_order = hands.keys.sort
+#hands_in_order.each {p _1}
+winnings = hands_in_order.map.with_index {|h,i| hands[h] * (hands.length-i)}
+p winnings.sum
