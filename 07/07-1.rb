3ec90e003dde38c5460152383f6eb6cc33abaf58
Quinn Daley <quinn@fishpercolator.co.uk>
2023-12-07 11:15:02 +0000

    Commit this cool calculator for normal poker rules, before I read the much simpler rules of the actual puzzle

--- 07/07-1.rb
+++ 07/07-1.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class Hand
-  TYPES = [/(.)\1{4}/, /(.)\1{3}/, /(.)\1\1(.)\2/, /(.)\1\1/, /(.)\1.?(.)\2/, /(.)\1/, /(.)/].freeze
+  TYPES = [/(.)\1{4}/, /(.)\1{3}/, /(.)\1\1(.)\2/, /(.)\1\1/, /(.)\1(.?)(.)\3/, /(.)\1/, /(.)/].freeze
   CRANKS = "AKQJT98765432".split('')
 
   attr_reader :cards
@@ -10,20 +10,30 @@ class Hand
     @cards = cards.split('').sort_by {CRANKS.index(_1)}.join
   end
 
-  # Determine the two or three ranks that will be used to score this hand -
-  # lower is better
+  # Returns a series of integers that can be used to score the hand - lower is better.
   def scores
     type = TYPES.each_index.find {TYPES[_1] =~ cards}
-    [type, CRANKS.index($1), CRANKS.index($2)]
+
+    # Type 4 has three capture groups, so handle it differently
+    winning_cards = (type == 4 ? [$1,$3] : [$1,$2]).compact
+    non_winning_cards = ($`+(type == 4 ? $2 : '')+$').split('').uniq
+    [type] + (winning_cards + non_winning_cards).map {CRANKS.index(_1)}
   end
 
   # Puts lower value hands at the start
   def <=>(other)
     scores <=> other.scores
   end
+
+  # For debugging
+  def inspect
+    types = %i[five four fullhouse three twopair pair highcard]
+    s = scores
+    "#{cards} #{types[s.shift]} #{s.map {CRANKS[_1]}.inspect}"
+  end
 end
 
 hands = ARGF.inject({}) {|h,l| l.split(' ').then {h[Hand.new _1] = _2.to_i; h}}
-winnings = hands.keys.sort.map.with_index {|h,i| hands[h] * (hands.length-i)}
-p winnings.sum
-
+hands.keys.each {p _1}
+# winnings = hands.keys.sort.map.with_index {|h,i| hands[h] * (hands.length-i)}
+# p winnings.sum
