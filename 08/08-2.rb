d4d4ca22aa0c64b1b79b23f3b97664adb8509402
Quinn Daley <quinn@fishpercolator.co.uk>
2023-12-08 09:11:22 +0000

    I guess my brain is working well today\!

--- 08/08-2.rb
+++ 08/08-2.rb
@@ -4,19 +4,23 @@ directions = ARGF.readline.chomp.chars
 ARGF.readline # blank line
 map = ARGF.inject({}) {/(\w+) = \((\w+), (\w+)\)/ =~ _2; _1.tap {|h| h[$1] = [$2,$3]}}
 
-# This is the algorithm for solving it the "normal" way - as in the example.
-# Of course, it runs for a really long time so it will need optimizing
-
 loc = map.keys.select {_1.end_with? 'A'}
-steps = 0
-queue = []
-until loc.all? {_1.end_with? 'Z'}
-  queue = directions.dup if queue.empty?
-  dir = queue.shift
-  loc.map! { map[_1][dir == 'R' ? 1:0] }
-  p loc
-  steps += 1
+
+# Each location has a "period" that is a static number of steps - find it
+def find_period(map, directions, l)
+  steps = 0
+  queue = []
+  loop do
+    queue = directions.dup if queue.empty?
+    return steps if l.end_with? 'Z'
+    l = map[l][queue.shift == 'R' ? 1:0]
+    steps += 1
+  end
 end
 
-p steps
+periods = loc.map {find_period map, directions, _1}
+
+# The lcm of these periods is the first time they all intersect
+p periods.reduce(:lcm)
+
 
